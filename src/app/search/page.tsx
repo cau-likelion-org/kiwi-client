8098df9650cabbbc0e0d971f043cc388572de1d7
rbgksqkr <rbgks1937@naver.com>
2023-11-08 02:48:47 +0900

    Feat: 검색결과 여러개 리스트로 렌더링(#4)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,25 +1,53 @@
 'use client';
 
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const SearchPage = () => {
+	const [searchResult, setSearchResult] = useState([
+		{
+			id: 0,
+			category: '11기 해커톤',
+			content:
+				'Lorem ipsum dolor sit amet consectetur. Velit at nibh rutrum ultrices. Vestibulum magna nunc euismod diam. Nisl bibendum neque dignissim leo commodo elit volutpat. Dictum facilisi blandit tincidunt gravida ut tellus cursus tempor ipsum. Purus accumsan sagittis facilisis mauris cras dolor interdum. Tempor volutpat tellus nibh quisque mi commodo orci sed tincidunt',
+			directory: '11기 > 11기 행사 > 해커톤',
+		},
+		{
+			id: 1,
+			category: '10기 해커톤',
+			content:
+				'Lorem ipsum dolor sit amet consectetur. Velit at nibh rutrum ultrices. Vestibulum magna nunc euismod diam. Nisl bibendum neque dignissim leo commodo elit volutpat. Dictum facilisi blandit tincidunt gravida ut tellus cursus tempor ipsum. Purus accumsan sagittis facilisis mauris cras dolor interdum. Tempor volutpat tellus nibh quisque mi commodo orci sed tincidunt',
+			directory: '10기 > 10기 행사 > 해커톤',
+		},
+		{
+			id: 2,
+			category: '9기 해커톤',
+			content:
+				'Lorem ipsum dolor sit amet consectetur. Velit at nibh rutrum ultrices. Vestibulum magna nunc euismod diam. Nisl bibendum neque dignissim leo commodo elit volutpat. Dictum facilisi blandit tincidunt gravida ut tellus cursus tempor ipsum. Purus accumsan sagittis facilisis mauris cras dolor interdum. Tempor volutpat tellus nibh quisque mi commodo orci sed tincidunt',
+			directory: '9기 > 9기 행사 > 해커톤',
+		},
+	]);
+
 	return (
 		<SearchBarSection>
 			<SearchBarWapper>
 				<Image src="/search_bar.png" alt="" width={540} height={70} priority />
 			</SearchBarWapper>
-			<SearchResult>
-				<Image src="/lion.png" alt="" width={95} height={122} />
-				<SearchResultBox>
-					<div>11기 해커톤</div>
-					<div>Lorem</div>
-					<div>
-						11기 {'>'} 11기 행사 {'>'} 해커톤
-					</div>
-				</SearchResultBox>
-			</SearchResult>
+			<SearchResultWrapper>
+				{searchResult.map((result) => (
+					<SearchResult key={result.id}>
+						<LionImageWrapper>
+							<Image src="/lion.png" alt="" width={45} height={60} />
+						</LionImageWrapper>
+						<SearchResultBox>
+							<div>{result.category}</div>
+							<SearchResultContent>{result.content}</SearchResultContent>
+							<div>{result.directory}</div>
+						</SearchResultBox>
+					</SearchResult>
+				))}
+			</SearchResultWrapper>
 		</SearchBarSection>
 	);
 };
@@ -28,27 +56,47 @@ const SearchBarSection = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	background-color: blue;
+	background-color: #4546f2;
+	height: 100vh;
 `;
 
 const SearchBarWapper = styled.div`
 	padding: 20px;
 `;
 
+const SearchResultWrapper = styled.div`
+	margin-top: 50px;
+`;
+
 const SearchResult = styled.div`
 	display: flex;
 	gap: 20px;
-	margin: 10px;
+	margin: 30px;
+`;
+
+const LionImageWrapper = styled.div`
+	display: flex;
+	align-items: center;
 `;
 
 const SearchResultBox = styled.div`
 	width: 50vw;
+	height: 15vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 	background-color: white;
 	border: 3px solid black;
 	border-radius: 10px;
-	border: 4px solid #000;
-	background-color: white;
 	padding: 15px;
+`;
+
+const SearchResultContent = styled.div`
+	height: 9vh;
+	overflow: hidden;
+	line-height: 1.4;
+	margin-top: 10px;
+	margin-bottom: 10px;
 `;
 
 export default SearchPage;
