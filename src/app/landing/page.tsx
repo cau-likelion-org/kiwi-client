848c9e2edbbd8cad62893ee865832dc4a16f1116
CJY <cjy3458@naver.com>
2023-12-01 00:19:25 +0900

    Feat: 랜딩 페이지 퍼블리싱 (#7)

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Landing from '@/components/landing/Landing';
 import React from 'react';
 
---
+++ src/components/landing/Landing.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import LandingSection1 from './LandingSection1';
+import LandingSection2 from './LandingSection2';
+import LandingSection3 from './LandingSection3';
+
+const Landing = () => {
+	return (
+		<>
+			<LandingSection1 />
+			<LandingSection2 />
+			<LandingSection3 />
+		</>
+	);
+};
+
+export default Landing;
