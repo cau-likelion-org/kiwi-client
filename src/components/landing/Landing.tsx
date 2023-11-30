0f8008dbd9fc9951725dd2cf7874cfdecfeb8f47
gominzip <ahaminji@naver.com>
2023-12-01 00:26:27 +0900

    chore : 파일 이동

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -3,15 +3,26 @@
 import LandingSection1 from './LandingSection1';
 import LandingSection2 from './LandingSection2';
 import LandingSection3 from './LandingSection3';
+import { styled } from 'styled-components';
 
 const Landing = () => {
 	return (
-		<>
+		<LandingSection>
 			<LandingSection1 />
 			<LandingSection2 />
 			<LandingSection3 />
-		</>
+		</LandingSection>
 	);
 };
 
 export default Landing;
+
+const LandingSection = styled.div`
+background: #4c4df5;
+height: fit-content;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+font-family: NeoDunggeunmo Pro;`;
