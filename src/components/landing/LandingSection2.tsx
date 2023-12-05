6e731cdee392622988eb9c45631cefc79d226d6b
CJY <cjy3458@naver.com>
2023-12-05 17:02:19 +0900

    Feat: 랜딩 이미지 수정 (#7)

--- src/components/landing/LandingSection2.tsx
+++ src/components/landing/LandingSection2.tsx
@@ -7,6 +7,10 @@ import styled from 'styled-components';
 const LandingSection2 = () => {
 	return (
 		<ImageWrapper>
+			<Box>
+				<Content></Content>
+				<StyledImage2 src="/img/recent.png" alt="닉네임 박스 이미지" fill priority />
+			</Box>
 			<StyledImage src="/img/landing2.png" alt={'랜딩'} fill priority></StyledImage>
 		</ImageWrapper>
 	);
@@ -14,8 +18,38 @@ const LandingSection2 = () => {
 
 export default LandingSection2;
 
+const Box = styled.div`
+	position: relative;
+	width: 38%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: max-content;
+`;
+
+const Content = styled.div`
+	z-index: 1;
+	position: absolute;
+	margin-top: 4rem;
+	height: 70%;
+	width: 90%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	gap: 3rem;
+`;
+
 const ImageWrapper = styled.div`
 	width: 100%;
+	position: relative;
+`;
+
+const StyledImage2 = styled(Image)`
+	position: relative !important;
+	height: unset !important;
+	object-fit: cover;
 `;
 
 const StyledImage = styled(Image)`
@@ -23,10 +57,15 @@ const StyledImage = styled(Image)`
 	height: fit-content;
 `;
 
+const ImageWrapper2 = styled.div`
+	width: 25rem;
+	height: 15rem;
+`;
+
 const ShortCuts1 = styled.div`
-	position: relative;
-	width: 15rem;
-	background-color: black;
+	background: url('/img/recent.svg');
+	background-repeat: no-repeat;
+	background-size: cover;
 `;
 
 const ShortCuts2 = styled.div`
