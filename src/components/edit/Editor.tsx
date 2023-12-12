835f1e172facb04d3869c274af350bc2e6c97e11
CJY <cjy3458@naver.com>
2023-12-12 19:26:21 +0900

    Feat: 편집 페이지 퍼블리싱(#24)

---
+++ src/components/edit/Editor.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { styled } from 'styled-components';
+import Image from 'next/image';
+import React from 'react';
+
+const Editor = () => {
+	return <div>Editor</div>;
+};
+
+export default Editor;
