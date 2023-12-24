0a1bb2a55b5758b376f591b0602a8dcd8d1e0036
rbgksqkr <rbgks1937@naver.com>
2023-12-25 01:32:30 +0900

    fix: 페이지 컴포넌트 use client 제거(#28)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,22 +1,15 @@
-'use client';
-
 import SearchBodySection from '@/components/search/SearchBodySection';
 import SearchHeadSection from '@/components/search/SearchHeadSection';
-import styled from 'styled-components';
 
 const SearchPage = () => {
 	const searchKeyword = '무엇을 검색할까요?';
 
 	return (
-		<SearchBarSection>
+		<>
 			<SearchHeadSection />
 			<SearchBodySection searchKeyword={searchKeyword} />
-		</SearchBarSection>
+		</>
 	);
 };
-
-const SearchBarSection = styled.div`
-	margin-top: 64px;
-`;
 
 export default SearchPage;
