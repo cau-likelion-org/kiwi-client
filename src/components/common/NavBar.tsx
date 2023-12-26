8d129f33e12ca5f5c58b70f900532fbf3c02fe9a
CJY <cjy3458@naver.com>
2023-12-27 02:17:30 +0900

    Feat: NavBar 버튼 추가

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -14,13 +14,14 @@ const NavBar = () => {
 			<RightWrapper>
 				<SearchWrapper>
 					<ImageWrapper>
-						<Image src="/img/🔍.png" alt={'search'} width={25} height={25} />
+						<Image src="/img/🔍.png" alt={'search'} width={25} height={25} style={{ cursor: 'pointer' }} />
 					</ImageWrapper>
 					<SearchForm type="header" />
 				</SearchWrapper>
 				<ButtonWrapper>
-					<Image src="/img/random.png" alt={'random'} width={50} height={50} style={{ cursor: 'pointer' }} />
-					<Image src="/img/login.png" alt={'login'} width={50} height={50} style={{ cursor: 'pointer' }} />
+					<Image src="/img/newPost.png" alt={'newPost'} width={32} height={40} style={{ cursor: 'pointer' }} />
+					<Image src="/img/random.png" alt={'random'} width={40} height={40} style={{ cursor: 'pointer' }} />
+					<Image src="/img/login.png" alt={'login'} width={33} height={40} style={{ cursor: 'pointer' }} />
 				</ButtonWrapper>
 			</RightWrapper>
 		</Wrapper>
@@ -51,30 +52,30 @@ const LeftWrapper = styled.div`
 
 const RightWrapper = styled.div`
 	display: flex;
-	width: 500px;
-	margin-right: 3%;
+	width: 60%;
+	margin-right: 1%;
 	justify-content: space-between;
 	align-items: center;
+	@media screen and (min-width: 1024px) {
+		width: 40%;
+	}
 `;
 
 const SearchWrapper = styled.div`
 	display: flex;
 	position: relative;
-	width: 250px;
-	margin-left: 10%;
-	/* margin-top: 3px; */
-	/* border-bottom: 2px solid black; */
-	padding: 5px;
+	width: 45%;
 `;
 
 const ImageWrapper = styled.div`
 	position: absolute;
 	top: 15%;
+	margin-right: 3rem;
 `;
 
 const ButtonWrapper = styled.div`
 	display: flex;
-	width: 150px;
+	width: 40%;
 	margin-top: 6px;
 	justify-content: space-between;
 	align-items: center;
