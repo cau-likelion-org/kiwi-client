3a92359f751f26ac9728af6f03bf33a355405709
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-12-30 03:22:53 +0900

    Feat: 문서 내용 불러오기 함수 작성(#52)

---
+++ src/apis/viewer.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const baseURL = `http://llwiki.p-e.kr:8000/`;
+
+// 문서 내용 불러오기
+export const getDocsContent = async (title : string) => {
+    try{
+        const response = await axios.get(`${baseURL}/recent/docs/${title}`);
+        if(response.status === 200){
+            return response.data;
+        }
+        else{
+            console.log('Error occurred while fetching backlinks');
+        }
+    }
+    catch(error){
+        console.error(error);
+    }
+};
