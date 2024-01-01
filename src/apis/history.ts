10f6a80f358be9c5756b07d68b8d61acb81acb91
CJY <cjy3458@naver.com>
2024-01-02 00:32:13 +0900

    FIX: BASE URL 오류 제거

--- src/apis/history.ts
+++ src/apis/history.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'process.env.NEXT_PUBLIC_SERVER_URL';
+const baseURL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 // 특정 문서의 전체 편집 목록 불러오기
 export const getDocHistories = async (title: string) => {
