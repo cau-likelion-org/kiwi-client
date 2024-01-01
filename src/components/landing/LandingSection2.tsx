e59d8366c34e5a4da908ba2ef7c674d8c4df48d5
CJY <cjy3458@naver.com>
2024-01-02 01:29:00 +0900

    Feat: 재배포용 랜딩 수정

--- src/components/landing/LandingSection2.tsx
+++ src/components/landing/LandingSection2.tsx
@@ -117,7 +117,7 @@ const Box = styled.div`
 	height: max-content;
 	margin-left: 3rem;
 	gap: 1rem;
-	z-index: 1;
+	z-index: 5;
 	top: 12rem;
 	@media screen and (min-width: 1024px) {
 		top: 16rem;
@@ -127,7 +127,7 @@ const Box = styled.div`
 `;
 
 const Content = styled.div`
-	z-index: 2;
+	z-index: 5;
 	position: absolute;
 	margin-top: 4rem;
 	width: 90%;
@@ -184,12 +184,16 @@ const ShortCutWrapper = styled.div`
 `;
 
 const ShortCuts1 = styled.div`
-	width: 15rem;
+	width: 17%;
+	height: fit-content;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	text-align: center;
 	position: relative;
+	margin-left: 1rem;
+	margin-top: 4rem;
+	z-index: 3;
 	.overlay {
 		z-index: 1;
 		position: absolute;
@@ -197,12 +201,13 @@ const ShortCuts1 = styled.div`
 		align-items: center;
 		justify-content: center;
 		color: #000;
-		font-size: 1.5rem;
+		width: 70%;
+		font-size: 1.4rem;
 		font-family: 'NeoDunggeunmo Pro';
 		font-style: normal;
 		font-weight: bold;
 		line-height: normal;
-		letter-spacing: 0rem;
+		letter-spacing: 0.4rem;
 		margin-bottom: 7px;
 		@media screen and (min-width: 1024px) {
 			width: 90%;
@@ -212,17 +217,22 @@ const ShortCuts1 = styled.div`
 	}
 	@media screen and (min-width: 1024px) {
 		width: 12%;
+		margin-top: 0rem;
 		font-size: 1.3rem;
 	}
 `;
 
 const ShortCuts2 = styled.div`
-	width: 15rem;
+	width: 17%;
+	height: fit-content;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	text-align: center;
 	position: relative;
+	margin-right: 1.2rem;
+	margin-top: 5rem;
+	z-index: 3;
 	.overlay {
 		z-index: 1;
 		position: absolute;
@@ -230,12 +240,13 @@ const ShortCuts2 = styled.div`
 		align-items: center;
 		justify-content: center;
 		color: #000;
-		font-size: 1.5rem;
+		width: 70%;
+		font-size: 1.4rem;
 		font-family: 'NeoDunggeunmo Pro';
 		font-style: normal;
 		font-weight: bold;
 		line-height: normal;
-		letter-spacing: 0rem;
+		letter-spacing: 0.4rem;
 		@media screen and (min-width: 1024px) {
 			width: 90%;
 			font-size: 1.7rem;
@@ -244,6 +255,7 @@ const ShortCuts2 = styled.div`
 	}
 	@media screen and (min-width: 1024px) {
 		width: 12%;
+		margin-top: 0rem;
 		font-size: 1.3rem;
 	}
 `;
@@ -255,6 +267,7 @@ const LionWrapper = styled.div`
 	align-items: center;
 	position: relative;
 	bottom: 7rem;
+	z-index: 3;
 	@media screen and (min-width: 1024px) {
 		bottom: 16rem;
 	}
