38c96746cc7b83ed6384ac3ad4c8f0ce325125d6
gominzip <ahaminji@naver.com>
2024-01-07 00:41:18 +0900

    chore : 로그인 과정에서 아톰 세팅 추가

--- src/components/login/GoogleLogin.tsx
+++ src/components/login/GoogleLogin.tsx
@@ -1,16 +1,18 @@
 'use client';
 import { postCode } from '@/apis/login';
-import {userEmailAtom, userNameAtom } from '@/app/recoilContextProvider';
+import {token, userEmailAtom, userNameAtom } from '@/app/recoilContextProvider';
 import { postCodeBody } from '@/types/request';
 import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import Loading from '../common/Loading';
+import LocalStorage from '@/utils/localStorage';
 
 const GoogleLogin = () => {
 	const route = useRouter();
 	const setUserEmail = useSetRecoilState(userEmailAtom);
 	const setUserName = useSetRecoilState(userNameAtom);
+	const setToken = useSetRecoilState(token)
 
 	let code: string | null = null;
 
@@ -30,6 +32,7 @@ const GoogleLogin = () => {
 				if(result.status==='200'){
 					setUserEmail(result.data.email);
 					setUserName(result.data.name);
+					setToken({access: LocalStorage.getItem('access'), refresh: LocalStorage.getItem('refresh')})
 					setTimeout(() => {
 						route.push('/');
 					}, 1000);
