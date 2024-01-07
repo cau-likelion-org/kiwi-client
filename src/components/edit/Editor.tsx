94f5502caf0070a35baf65d73b3c9ec39fa1c1e2
CJY <cjy3458@naver.com>
2024-01-07 19:33:06 +0900

    FIX:  툴바 커스텀 이미지 버튼 위치 변경

--- src/components/edit/Editor.tsx
+++ src/components/edit/Editor.tsx
@@ -1,6 +1,6 @@
 import '@uiw/react-md-editor/markdown-editor.css';
 import '@uiw/react-markdown-preview/markdown.css';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { ICommand, commands } from '@uiw/react-md-editor';
 import MDEditor from '@uiw/react-md-editor';
@@ -67,7 +67,7 @@ const Editor: React.FC = () => {
 		keyCommand: 'image-upload',
 		buttonProps: { 'aria-label': 'Upload image', title: 'Upload image' },
 		icon: (
-			<svg width="14" height="14" viewBox="0 0 20 20">
+			<svg width="14" height="14" viewBox="0 0 20 15">
 				<path
 					fill="currentColor"
 					d="M15 9c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm4-7H1c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h18c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 13l-6-5-2 2-4-5-4 8V4h16v11z"
