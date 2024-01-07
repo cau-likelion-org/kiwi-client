6180d29ff81697a67acc02ee1264a4e5927c6936
gominzip <ahaminji@naver.com>
2024-01-07 20:06:34 +0900

    feat : 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,36 @@ import type { Metadata } from 'next';
 import RecoidContextProvider from './recoilContextProvider';
 
 export const metadata: Metadata = {
-	title: '멋사 위키',
-	description: '멋쟁이 사자처럼 위키 페이지',
+	// verification: {
+	// 	google: 'EOz8bF08VwtMDtyyB7n5kcMxUo5ARoeoIqnIeC8_haM',
+	// },
+	title: '멋사 중앙대 위키',
+	description: '멋쟁이 사자처럼 중앙대학교의 위키 입니다',
+	authors: [{ name: '중하하 2기' }],
+	creator: '중하하 2기',
+	publisher: '중하하 2기',
 	icons: {
 		icon: '/img/icon.png',
 	},
-	keywords: ['멋쟁이 사자처럼', '멋사', '위키', '중앙대학교'],
-	creator: 'JayJay03',
+	keywords: [
+		'중앙대학교',
+		'멋쟁이 사자처럼',
+		'멋사',
+		'멋사 위키',
+		'중앙대학교 멋사'
+	],
+	openGraph: {
+		title: '멋사 중앙대 위키',
+		description: '멋쟁이 사자처럼 중앙대학교의 위키 입니다',
+		url: 'https://kiwi-client.vercel.app/',
+		siteName: '멋사 중앙대 위키',
+		// images: [
+		// 	{
+		// 		url: 'https://kiwi-client.vercel.app/어쩌구.png',
+		// 	},
+		// ],
+		type: 'website',
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
