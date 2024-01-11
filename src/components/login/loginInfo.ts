eb94cfdab25401440bda106db81ab48d7d2298e8
gominzip <ahaminji@naver.com>
2024-01-11 18:36:52 +0900

    chore : 리다이렉트 변경

--- src/components/login/loginInfo.ts
+++ src/components/login/loginInfo.ts
@@ -1,4 +1,4 @@
 // const redirect_URI = `http://localhost:3000/googlelogin`
-const redirect_URI = `https://kiwi-client.vercel.app/googlelogin`
+const redirect_URI = `https://wiki.cau-likelion.org/googlelogin`
 const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
 export const google_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${redirect_URI}&response_type=code&scope=email`
