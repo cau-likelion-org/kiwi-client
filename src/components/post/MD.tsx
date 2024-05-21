28c734aaaf2119d4bd3a09b1c9b95eb7df1bfd4b
CJY <cjy3458@naver.com>
2024-05-21 22:35:18 +0900

    feat: 토글 기능 구현

--- src/components/post/MD.tsx
+++ src/components/post/MD.tsx
@@ -52,6 +52,45 @@ const Upload: React.FC = () => {
 		return URL.createObjectURL(file);
 	}
 
+	const ToggleCommand: ICommand = {
+		name: 'Toggle',
+		keyCommand: 'Toggle',
+		buttonProps: { 'aria-label': 'Insert expander', title: 'Toggle' },
+		icon: (
+			<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 172 172">
+				<g
+					fill="none"
+					fill-rule="nonzero"
+					stroke="none"
+					stroke-width="1"
+					stroke-linecap="butt"
+					stroke-linejoin="miter"
+					stroke-miterlimit="10"
+					stroke-dasharray=""
+					stroke-dashoffset="0"
+					font-family="none"
+					font-weight="none"
+					font-size="none"
+					text-anchor="none"
+				>
+					<path d="M0,172v-172h172v172z" fill="none"></path>
+					<g fill="#000000">
+						<path d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"></path>
+					</g>
+				</g>
+			</svg>
+		),
+		execute: (state, api) => {
+			const expanderHtml = `
+	<details>
+	  <summary>토글 제목</summary>
+	  여기에 글을 입력하세요
+	</details>
+	`;
+			api.replaceSelection(expanderHtml);
+		},
+	};
+
 	const imageUploadCommand: ICommand = {
 		name: 'image-upload',
 		keyCommand: 'image-upload',
@@ -90,7 +129,7 @@ const Upload: React.FC = () => {
 				<Input value={title} onChange={inputChange} placeholder="문서 제목을 입력하세요" />
 				<div className="markarea">
 					<MDEditor
-						commands={[...customCommands, imageUploadCommand]}
+						commands={[...customCommands, imageUploadCommand, ToggleCommand]}
 						value={md}
 						onChange={handleEditorChange}
 						height={500}
