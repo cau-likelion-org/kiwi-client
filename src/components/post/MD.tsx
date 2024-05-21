a9741ece2a9b90909aa58f003213b6c1d6f0d5f3
CJY <cjy3458@naver.com>
2024-05-21 22:47:48 +0900

    feat: 토글기능 구현

--- src/components/post/MD.tsx
+++ src/components/post/MD.tsx
@@ -52,7 +52,35 @@ const Upload: React.FC = () => {
 		return URL.createObjectURL(file);
 	}
 
-	const ToggleCommand: ICommand = {
+	const imageUploadCommand: ICommand = {
+		name: 'image-upload',
+		keyCommand: 'image-upload',
+		buttonProps: { 'aria-label': 'Upload image', title: 'Upload image' },
+		icon: (
+			<svg width="14" height="14" viewBox="0 0 20 15">
+				<path
+					fill="currentColor"
+					d="M15 9c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm4-7H1c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h18c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 13l-6-5-2 2-4-5-4 8V4h16v11z"
+				></path>
+			</svg>
+		),
+		execute: (state, api) => {
+			const fileInput = document.createElement('input');
+			fileInput.type = 'file';
+			fileInput.accept = 'image/*';
+			fileInput.onchange = async () => {
+				if (!fileInput.files?.length) return;
+				const [file] = Array.from(fileInput.files);
+				const blobUrl = createBlobUrl(file);
+				const imageUrl = await uploadImageToServer(blobUrl);
+				const imageMarkdown = `<img src="${imageUrl}" alt="image" style="width: 30%;"/>`;
+				api.replaceSelection(imageMarkdown);
+			};
+			fileInput.click();
+		},
+	};
+
+	const toggleCommand: ICommand = {
 		name: 'Toggle',
 		keyCommand: 'Toggle',
 		buttonProps: { 'aria-label': 'Insert expander', title: 'Toggle' },
@@ -91,34 +119,6 @@ const Upload: React.FC = () => {
 		},
 	};
 
-	const imageUploadCommand: ICommand = {
-		name: 'image-upload',
-		keyCommand: 'image-upload',
-		buttonProps: { 'aria-label': 'Upload image', title: 'Upload image' },
-		icon: (
-			<svg width="14" height="14" viewBox="0 0 20 15">
-				<path
-					fill="currentColor"
-					d="M15 9c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm4-7H1c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h18c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 13l-6-5-2 2-4-5-4 8V4h16v11z"
-				></path>
-			</svg>
-		),
-		execute: (state, api) => {
-			const fileInput = document.createElement('input');
-			fileInput.type = 'file';
-			fileInput.accept = 'image/*';
-			fileInput.onchange = async () => {
-				if (!fileInput.files?.length) return;
-				const [file] = Array.from(fileInput.files);
-				const blobUrl = createBlobUrl(file);
-				const imageUrl = await uploadImageToServer(blobUrl);
-				const imageMarkdown = `<img src="${imageUrl}" alt="image" style="width: 30%;"/>`;
-				api.replaceSelection(imageMarkdown);
-			};
-			fileInput.click();
-		},
-	};
-
 	return (
 		<>
 			<Wrapper>
@@ -129,7 +129,7 @@ const Upload: React.FC = () => {
 				<Input value={title} onChange={inputChange} placeholder="문서 제목을 입력하세요" />
 				<div className="markarea">
 					<MDEditor
-						commands={[...customCommands, imageUploadCommand, ToggleCommand]}
+						commands={[...customCommands, imageUploadCommand, toggleCommand]}
 						value={md}
 						onChange={handleEditorChange}
 						height={500}
