b7d9a4e27fc03b4168fae8aeee38f9480fa602a4
rbgksqkr <rbgks1937@naver.com>
2024-06-19 16:59:42 +0900

    fix: 문서 페이지로 이동해도 Navbar 입력값 유지(#120)

--- src/hooks/useKeywordParams.ts
+++ src/hooks/useKeywordParams.ts
@@ -3,7 +3,7 @@ import { useSearchParams } from 'next/navigation';
 const useKeywordParams = () => {
 	const params = useSearchParams();
 
-	const searchKeyword = params.get('search') || '멋쟁이사자처럼';
+	const searchKeyword = params.get('search') || params.get('title') || '멋쟁이사자처럼';
 
 	return { searchKeyword };
 };
