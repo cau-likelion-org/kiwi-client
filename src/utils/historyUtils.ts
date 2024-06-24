9f64ddacd09a58005c557b17bd5d9c39d5b5f310
gominzip <ahaminji@naver.com>
2024-06-24 18:46:32 +0900

    refactor : 문서 역사 이미지 태그 파싱 추가 (#122)

--- src/utils/historyUtils.ts
+++ src/utils/historyUtils.ts
@@ -1,66 +1,74 @@
-export const renderOldStr = (change: any) => {
-    var oldStr = change.replace(
-        /<modified_to>(.*?)<\/modified_to>|<added>(.*?)<\/added>/g,
-        function ([match, p1, p2]: any) {
-            return p1 ? ' ' : p2 ? ' ' : '';
-        },
-    );
-
-    oldStr = oldStr.replace(/<added>/g, ' ' + "<span class='none'>");
-    oldStr = oldStr.replace(/<\/added>/g, '</span>');
-    oldStr = oldStr.replace(/<modified_to>/g, ' ' + "<span class='none'>");
-    oldStr = oldStr.replace(/<\/modified_to>/g, '</span>');
-
-    oldStr = oldStr.replace(/<deleted>/g, ' ' + "<span class='delete'>");
-    oldStr = oldStr.replace(/<\/deleted>/g, '</span>');
-    oldStr = oldStr.replace(/<modified_from>/g, ' ' + "<span class='from'>");
-    oldStr = oldStr.replace(/<\/modified_from>/g, '</span>');
-
-    oldStr = oldStr.replace(/\r\n|\n|\r|\\r\\n|\\n|\\r/g, '<br/>');
-
-    return { __html: oldStr };
+export const cleanImageTag = (str: string): string => {
+	return str
+		.replace(/<img\s+([^>]*?)\s*>/g, (match: string, p1: string) => {
+			p1 = p1.replace(/(src\s*=\s*["'])(.*?)(["'])/g, (match: string, prefix: string, url: string, suffix: string) => {
+				return prefix + url.replace(/\s+/g, '') + suffix;
+			});
+			return `<img ${p1.replace(/\s*=\s*/g, '=').replace(/\s+/g, ' ')}>`;
+		})
+		.replace(/"\s/g, '"')
+		.replace(/\s"/g, '"')
+		.trim();
 };
 
-export const renderNewStr = (change: any) => {
-    let newStr = change.replace(
-        /<modified_from>(.*?)<\/modified_from>|<deleted>(.*?)<\/deleted>/g,
-        function (): string {
-            return '';
-        },
-    );
+export const renderOldStr = (change: string): { __html: string } => {
+	let oldStr = change.replace(/<modified_to>(.*?)<\/modified_to>|<added>(.*?)<\/added>/g, (match, p1, p2) =>
+		p1 ? ' ' : p2 ? ' ' : '',
+	);
 
-    newStr = newStr.replace(/<deleted>/g, ' ' + "<span class='none'>");
-    newStr = newStr.replace(/<\/deleted>/g, '</span>');
-    newStr = newStr.replace(/<modified_from>/g, ' ' + "<span class='none'>");
-    newStr = newStr.replace(/<\/modified_from>/g, '</span>');
+	oldStr = oldStr
+		.replace(/<added>/g, " <span class='none'>")
+		.replace(/<\/added>/g, '</span>')
+		.replace(/<modified_to>/g, " <span class='none'>")
+		.replace(/<\/modified_to>/g, '</span>')
+		.replace(/<deleted>/g, " <span class='delete'>")
+		.replace(/<\/deleted>/g, '</span>')
+		.replace(/<modified_from>/g, " <span class='from'>")
+		.replace(/<\/modified_from>/g, '</span>')
+		.replace(/\r\n|\n|\r|\\r\\n|\\n|\\r/g, '<br/>');
 
-    newStr = newStr.replace(/<added>/g, ' ' + "<span class='add'>");
-    newStr = newStr.replace(/<\/added>/g, '</span>');
-    newStr = newStr.replace(/<modified_to>/g, ' ' + "<span class='to'>");
-    newStr = newStr.replace(/<\/modified_to>/g, '</span>');
+	oldStr = cleanImageTag(oldStr);
 
-    newStr = newStr.replace(/\r\n|\n|\r|\\r\\n|\\n|\\r/g, '<br/>');
-
-    return { __html: newStr };
-};
-export const renderFirstStr = (content: any) => {
-    let firstStr = content;
-
-    firstStr = firstStr.replace(/\r\n|\n|\r|\\r\\n|\\n|\\r/g, '<br/>');
-    firstStr = "<span class='add'>" + firstStr + '</span>';
-
-    return { __html: firstStr };
+	return { __html: oldStr };
 };
 
-export const parseAndFormatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        weekday: 'short',
-        hour: 'numeric',
-        minute: 'numeric',
-    };
-    return date.toLocaleString('ko-KR', options);
+export const renderNewStr = (change: string): { __html: string } => {
+	let newStr = change.replace(/<modified_from>(.*?)<\/modified_from>|<deleted>(.*?)<\/deleted>/g, () => '');
+
+	newStr = newStr
+		.replace(/<deleted>/g, " <span class='none'>")
+		.replace(/<\/deleted>/g, '</span>')
+		.replace(/<modified_from>/g, " <span class='none'>")
+		.replace(/<\/modified_from>/g, '</span>')
+		.replace(/<added>/g, " <span class='add'>")
+		.replace(/<\/added>/g, '</span>')
+		.replace(/<modified_to>/g, " <span class='to'>")
+		.replace(/<\/modified_to>/g, '</span>')
+		.replace(/\r\n|\n|\r|\\r\\n|\\n|\\r/g, '<br/>');
+
+	newStr = cleanImageTag(newStr);
+
+	return { __html: newStr };
+};
+
+export const renderFirstStr = (content: string): { __html: string } => {
+	let firstStr = content.replace(/\r\n|\n|\r|\\r\\n|\\n|\\r/g, '<br/>');
+	firstStr = `<span class='add'>${firstStr}</span>`;
+
+	firstStr = cleanImageTag(firstStr);
+
+	return { __html: firstStr };
+};
+
+export const parseAndFormatDate = (dateString: string): string => {
+	const date = new Date(dateString);
+	const options: Intl.DateTimeFormatOptions = {
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric',
+		weekday: 'short',
+		hour: 'numeric',
+		minute: 'numeric',
+	};
+	return date.toLocaleString('ko-KR', options);
 };
