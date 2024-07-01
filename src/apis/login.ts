b725e1f850d10774f9bad534a9f44d8468b8b298
gominzip <ahaminji@naver.com>
2024-07-02 00:46:08 +0900

    feat : refresh토큰을 통한 access토큰 재발급 함수 구현 (#119)

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -18,71 +18,36 @@ export const postCode = async (body: postCodeBody) => {
 		}
 		return response.data;
 	} catch (error) {
-		console.error('에러 발생', error);
+		// console.error('에러 발생', error);
 		throw error;
 	}
 };
 
 // 토큰 재발급
-// export const getNewRefreshToken = async () => {
-// 	const accessToken = localStorage.getItem("access");
-// 	const refreshToken = localStorage.getItem("refresh");
-// 	const result = await axios.post(
-// 	  "http://front.cau-likelion.org/refresh",
-// 	  {
-// 		// 요청 body
-// 		refreshToken,
-// 	  },
-// 	  {
-// 		// headers.Authorization 필수
-// 		headers: {
-// 		  Authorization: accessToken,
-// 		},
-// 	  }
-// 	);
-// 	console.log(result)
-// 	return result.data;
-//   };
-
-// 회원가입
-export const signUp = async (emailInput: string, nameInput: string) => {
-	// 구글 로그인 후 context에 이메일 저장하고 있음
+export const getNewToken = async () => {
+	const refreshToken = localStorage.getItem('refresh');
 	try {
-		const response = await axios.post(`${baseURL}users/signup/`, { email: emailInput, name: nameInput });
-		if (response.data.status === '201') {
-			const accessToken = response.data.data.token.access_token;
-			const refreshToken = response.data.data.token.refresh_token;
+		const response = await axios.post(
+			`${baseURL}users/token/refresh/`,
+			{
+				refresh: refreshToken,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${refreshToken}`,
+				},
+			},
+		);
 
-			LocalStorage.setItem('access', accessToken);
-			LocalStorage.setItem('refresh', refreshToken);
+		if (response.status === 200) {
+			const newAccessToken = response.data.access;
+			return {
+				accessToken: newAccessToken,
+			};
+		} else {
+			throw new Error('토큰 갱신 실패');
 		}
-		return response.data;
 	} catch (error) {
-		console.log(error);
-		return false;
-	}
-};
-
-// 랜덤 닉네임 부여
-export const getRandomNickname = async () => {
-	try {
-		const response = await axios.get(`${baseURL}users/rand-name/`);
-		return response.data;
-	} catch (error) {
-		console.log(error);
-		return false;
-	}
-};
-
-// 닉네임 중복여부 확인
-export const checkNickName = async (name: string) => {
-	var resultString = name.replace(/\s+/g, '+');
-	try {
-		const response = await axios.get(`${baseURL}users/check-name/?name=${resultString}`);
-		return response.data;
-	} catch (error) {
-		// 사용할 수 없는 닉네임
-		console.log(error);
-		return false;
+		throw new Error('토큰 갱신 요청 실패');
 	}
 };
