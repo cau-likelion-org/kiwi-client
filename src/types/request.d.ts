c7e32b69c823c9d37b7877d799484aa2efaabbe9
CJY <cjy3458@naver.com>
2025-03-05 17:27:31 +0900

    fix: 13기 업뎅트

--- src/types/request.d.ts
+++ src/types/request.d.ts
@@ -1,6 +1,6 @@
 // type 정의
 
-export type Generation = '9기' | '10기' | '11기' | '12기';
+export type Generation = '9기' | '10기' | '11기' | '12기' | '13기';
 
 export interface IOption {
 	value: Generation;
